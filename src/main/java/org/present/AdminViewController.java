5b96b70e9c2477321c11bde4e4377b0ee1c25732
Schachster <anni619@student.sdu.dk>
2020-05-18 14:40:35 +0200

    Implemented CreateUser.fxml
    as well as updates to admin view to redirect to CreateUser

--- src/main/java/org/present/AdminViewController.java
+++ src/main/java/org/present/AdminViewController.java
@@ -1,4 +1,11 @@
 package org.present;
 
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
 public class AdminViewController {
+    public void switchToCreateUser(ActionEvent actionEvent) throws IOException {
+        App.setRoot("CreateUser");
+    }
 }
